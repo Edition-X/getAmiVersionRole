95b7d5317dbb631695de607719f65214c8657205
Maxeda Beheer <maxedabeheer@mirabeau.nl>
2016-05-03 14:11:17 +0200

    Added lambda function -  not fully working yet
    Signed-off-by: Maxeda Beheer <maxedabeheer@mirabeau.nl>

--- getAmiVersionRole_lambda.py
+++ getAmiVersionRole_lambda.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python
 from __future__ import print_function
-
 import boto3
 
-def getAmiForVersion(version, role):
-    ec2client = boto3.client('ec2')
 
-    filters = [{'Name':'tag:imh:version', 'Values':[version]}]
-    if role:
-        filters += [{'Name':'tag:imh:role', 'Values':[role]}]
+def getAmiForVersionRole(role, version = None):
+    ec2client = boto3.client('ec2')
+    filters = [{'Name':'tag:imh:role', 'Values':[role]}]
+    if version:
+        filters += [{'Name':'tag:imh:version', 'Values':[version]}]
     targetAmi = ec2client.describe_images(
            Filters = filters
     )
     if targetAmi and len(targetAmi['Images'] and not role):
-        return Exception('No AMI found for Version: {0}'.format(version))
+        return Exception('No AMI found for version: {0}'.format(version))
+    return targetAmi
 
-    if targetAmi and len(targetAmi['Images'] and role):
-        return Exception('No AMI found for Version: {0} and Role: {1}'.format(version, role))
+
+def extractVersion(targetAmi):
+    version = ''
+    for key, value in targetAmi.iteritems():
+        if key == 'Images':
+            for item in value:
+                for element in item['Tags']:
+                    if element['Key'] == 'imh:version':
+                        version = element['Value']
+    return version
+
+def extractData(targetAmi, version):
+    versionToImageId = {}
+    versionList = []
+    for key, value in targetAmi.iteritems():
+        if key == 'Images':
+            for item in value:
+                versionToImageId['Name'] = version
+                versionToImageId['Value'] = item['ImageId']
+                versionList.append(versionToImageId)
+    return versionList
+
 
 def lambda_handler(event, context):
-    cloudformation = boto3.resource('cloudformation')
-    stack = cloudformation.Stack(event['stackname'])
-    amiId = getAmiForVersion(event['applicationVersion'], event['applicationRole'])
-    
-    parameters = stack.parameters
-    tags = stack.tags
+    ami = getAmiForVersionRole(event['applicationVersion'], event['applicationRole'])
+    version = extractVersion(ami)
+    return extractData(ami, version)
 
-    for i, param in enumerate(parameters):
-        if parameters[i]['ParameterKey'] == 'HybrisserverAMI':
-            parameters[i]['ParameterValue'] = amiId
-    
-    for i, tag in enumerate(tags):
-        if tags[i]['Key'] == 'imh:stack:application-version':
-            tags[i]['Value'] = event['applicationVersion']
-
-    for i, tag in enumerate(tags):
-        if tags[i]['Key'] == 'imh:stack:roles':
-            tags[i]['Value'] = event['applicationRole']
-
-    response = stack.update(
-        UsePreviousTemplate=True,
-        Parameters = parameters,
-        Tags = tags,
-    )
+#userVersion = '8.10.0-release-1'
+#amiVersion = getAmiForVersionRole('hybris', userVersion)
+#version = extractVersion(amiVersion)
+#print('List Version: {0} \n\n'.format(extractData(amiVersion, version)))
